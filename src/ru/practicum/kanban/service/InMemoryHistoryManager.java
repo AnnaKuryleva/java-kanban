5cc7e73665585bbeab923f5558399ded63d2faf6
Anna Kuryleva <annak12111974@gmail.com>
2024-12-06 17:18:49 +0300

    implement a change in logic when saving tasks to history. update the tests

--- src/ru/practicum/kanban/service/InMemoryHistoryManager.java
+++ src/ru/practicum/kanban/service/InMemoryHistoryManager.java
@@ -3,9 +3,10 @@ package ru.practicum.kanban.service;
 import ru.practicum.kanban.model.Task;
 
 import java.util.*;
+
 public class InMemoryHistoryManager implements HistoryManager {
     private final Map<Integer, Node> historyMap = new HashMap<>();
-    Node<Task> head ;
+    Node<Task> head;
     Node<Task> tail;
 
     @Override
@@ -28,15 +29,15 @@ public class InMemoryHistoryManager implements HistoryManager {
     public void remove(int id) {
         Node<Task> node = historyMap.get(id);
         if (node == null) {
-           return;
+            return;
         }
         historyMap.remove(id);
         Node<Task> prevNode = node.prev;
         Node<Task> nextNode = node.next;
         if (prevNode != null) {
             prevNode.next = nextNode;
-        } else  {
-           head = nextNode;
+        } else {
+            head = nextNode;
         }
         if (nextNode != null) {
             nextNode.prev = prevNode;
@@ -48,13 +49,13 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public List<Task> getHistoryTasks() {
         List<Task> historyTasks = new ArrayList<>();
-        for(Node<Task> node : historyMap.values()) {
+        for (Node<Task> node : historyMap.values()) {
             historyTasks.add(node.task);
         }
         return historyTasks;
     }
 
-     class Node<Task> {
+    private class Node<Task> {
         Task task;
         Node<Task> next;
         Node<Task> prev;
